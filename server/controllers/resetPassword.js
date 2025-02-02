52f3dd9c5df24f573f98b6ee6c3869f3672c7aff
ishanupadhyay9 <ishanupadhyay1000@gmail.com>
2025-02-02 23:58:16 +0530

    Update resetPassword.js

--- server/controllers/resetPassword.js
+++ server/controllers/resetPassword.js
@@ -24,7 +24,7 @@ exports.resetPasswordToken = async (req, res) => {
     )
     console.log("DETAILS", updatedDetails)
 
-    const url = `http://localhost:3000/update-password/${token}`
+    const url = `https://tech-horizon-complete.vercel.app/update-password/${token}`
     
 
     await mailSender(
